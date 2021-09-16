b89d949af283c80bbc94476d0db9de31064a8a21
Zemeng Li <lzm505553681@gmail.com>
2021-09-16 17:02:04 -0400

    update 9/16 17:00
    Moving basket & dropping apple complete

--- Assets/01-Apple Picker/Scripts/Apple.cs
+++ Assets/01-Apple Picker/Scripts/Apple.cs
@@ -2,17 +2,23 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class Apple : MonoBehaviour
 {
     // Start is called before the first frame update
+
+    public static float bottomY = -20f;
+
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (transform.position.y < bottomY)
+        {
+            Destroy(this.gameObject);
+        }
     }
 }
